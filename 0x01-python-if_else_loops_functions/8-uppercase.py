6bd671a112fc7a734a5504451f571f08fee25319
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-10-03 14:49:08 -0700

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def to_uper(char): #char = character
+    if ord(char) >= 97 and ord(char) <= 122:
+        return (ord(char) - 32)
+    else:
+        return ord(char)
+
+def uppercase(str):
+    new = ""
+    for char in str:
+        new += "%c" % to_uper(char)
+    print("{:s}".format(new))
