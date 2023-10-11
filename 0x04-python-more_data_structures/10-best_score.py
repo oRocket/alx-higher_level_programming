9aea35c7c5c76d109eaf33571bfd4926fe9ffa36
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-10-11 13:46:48 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_score = max(a_dictionary.values())
-        best_student = [key for key, value in a_dictionary.items() if value == max_score]
-        return best_student[0]
-    return None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
