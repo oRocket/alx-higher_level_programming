4fadfbdabadf57992b2572f65772c4e78140b0f6
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-10-24 14:58:12 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,12 +2,12 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
         """
