b320044e98b64120bc8bab16d4f21cababdff642
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-12 16:41:33 -0800

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- A script that lists the number of records with the same score in the table second_table in my MySQL server.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
