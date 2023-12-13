cadb5edc295f6eaa50c4ff4cc31c5cfeed324f0f
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-12 16:47:47 -0800

    Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- A script that displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
