d2ea5a5365ff531160f896feff9924bcaa3a26a5
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-08 13:23:35 -0800

    First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// 0-javascript_is_amazing.js
+
+// Create a constant variable called myVar
+const myVar = 'JavaScript is amazing';
+
+// Use console.log(...) to print the output
+console.log(myVar);
