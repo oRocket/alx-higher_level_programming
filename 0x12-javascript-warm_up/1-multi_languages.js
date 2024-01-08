2a3177db1be61fdab3e4a96b9f80ce7bd64059f1
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-08 13:30:27 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-// Use console.log(...) to print the three lines
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
