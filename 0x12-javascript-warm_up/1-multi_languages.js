092552ec3a486a0239900b55794028dd2e925202
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-08 13:28:00 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// 1-multi_languages.js
+
+// Use console.log(...) to print the three lines
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
