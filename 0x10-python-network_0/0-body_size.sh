4ca5705d97a152d7fcabb493fc503c2eb20d6974
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-25 14:53:24 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to get the body size in bytes
-size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-
-# Display the size
-echo "$size"
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
